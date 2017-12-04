60180c61ac1a8b76a9a98842e364f573f9bced82
lizhipeng <lizhipeng@gmail.com>
2017-12-04 23:03:58 +0800

    Add test

---
+++ src/test/java/cn/lzhp/test/TestAutoService.java
@@ -0,0 +1,36 @@
+package cn.lzhp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cn.lzhp.tools.service.AutoService;
+
+public class TestAutoService {
+
+	@Test
+	public void test() throws InterruptedException {
+		AutoService service = new AutoService();
+
+		service.setServiceName("EntryService");
+		service.setAccessorIdleSeconds(5);
+		service.setAccessorName("cn.lzhp.test.EntryService");
+		service.setAccessorParams("");
+		service.setAccessorThreadCounts(2);
+
+		service.setProcessorIdleSeconds(1);
+		service.setProcessorName("cn.lzhp.test.EntryService");
+		service.setProcessorParams("");
+		service.setProcessorThreadCounts(4);
+
+		service.setReleaserName("cn.lzhp.test.EntryService");
+		service.setReleaserParams("");
+
+		service.start();
+
+		Thread.sleep(10 * 1000);
+
+		service.stop();
+	}
+
+}
